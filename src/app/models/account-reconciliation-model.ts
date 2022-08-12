bce83cf212f16f5b42703759c12123d3491f60ce
egemenozyurek <ozyurekegemen95@gmail.com>
2022-08-12 23:34:18 +0100

    after long break, I'm back :D

---
+++ src/app/models/account-reconciliation-model.ts
@@ -0,0 +1,18 @@
+export interface AccountReconciliationModel {
+    id: number;
+    companyId: number;
+    currencyAccountId: number;
+    startingDate: string;
+    endingDate: string;
+    currencyId: number;
+    currencyDebit: number;
+    currencyCredit: number;
+    isSendEmail: boolean;
+    sendEmailDate: string;
+    isEmailRead: boolean;
+    emailReadDate: string;
+    isResultSucceed: boolean;
+    resultDate: string;
+    resultNote: string;
+    guid: string;
+}
